3055a96d1a1281addf822229a647e70c86b3b89a
Matheus Filipe de Deus <161864558+0matheusfilipe@users.noreply.github.com>
2024-07-03 23:30:40 -0300

    Create Tabuleiro.cs

---
+++ Tabuleiro.cs
@@ -0,0 +1,7 @@
+class Tabuleiro
+{
+    public const int TamanhoPista = 52;
+    public const int TamanhoCasaFinal = 6;
+    public static int[] CasasSeguras = { 9, 22, 35, 48 };
+    public static int[] CasasIniciais = { 1, 14, 27, 40 };
+}
